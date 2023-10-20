7c73bc21eee6f9337e286698da5a295a1dec2658
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-10-20 19:30:28 +0200

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,76 @@
-import { Component } from 'react';
+
 import { nanoid } from 'nanoid';
 import { AddForm } from './AddForm/AddForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
+import { useState, useEffect } from 'react';
+import Notiflix from 'notiflix';
 
-
-export class App extends Component {
-  
-  state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: ''
-  }
-  
-
-  componentDidMount() {
+const getContacts = () => {
     const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      })
-    }
-  };
+      if (savedContacts !== null) {
+        return JSON.parse(savedContacts);
+      }
+    return [
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+}
 
-  componentDidUpdate(_, prevState) {
-
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  };
-
-
-  onFormSubmit = (data) => {
-
-    const checkName = this.state.contacts.find(
-					contact => contact.name.toLowerCase() === data.name.toLowerCase());
-    if (checkName) {
-      return alert(`${data.name} is already in contacts` )
-    }
+export const App = () => {
   
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, {...data, id:nanoid()} ],
-    }));
-  }
+    const [contacts, setContacts] = useState(getContacts);
+  
+    const [filter, setFilter] = useState('');
 
 
-  onClickDel = (delId) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== delId)
-    }))
-  }
+    useEffect(() => {
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+    }, [contacts]);
+  
+
+    const onFormSubmit = (data) => {
+      const checkName = contacts.find(
+        contact => contact.name.toLowerCase() === data.name.toLowerCase());
+      if (checkName) {
+        return Notiflix.Notify.failure(`${data.name} is already in contacts`);
+      }
+
+      setContacts(prevContacts => (
+        [...prevContacts, {...data, id:nanoid()} ]
+      ))
+    }
 
 
-  onChangeFilter = (filterName) => {
-    this.setState ({
-      filter: filterName
-    })
-  }
+    const onClickDel = (delId) => {
+      setContacts(prevContacts => (
+        prevContacts.filter(item => item.id !== delId)
+      ))
+    }
 
 
-  getVisibleItems = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter(item => {
-      const nameFilter = filter.toLowerCase();
-      return item.name.toLowerCase().includes(nameFilter);
-    });
-  };
+    const onChangeFilter = (filterName) => {
+      setFilter(filterName)
+    }
 
   
-  render() {
-    const visibleItems = this.getVisibleItems();
+    const visibleItems = contacts.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()))
+
     
     return (
       <div className='main-div'>
         <h1>Phonebook</h1>
-        <AddForm onFormSubmit={this.onFormSubmit} />
+        <AddForm onFormSubmit={onFormSubmit} />
         <h2>Contacts</h2>
-        <Filter onChangeFilter={this.onChangeFilter} />
+        <Filter onChangeFilter={onChangeFilter} />
         <ContactsList
           items={visibleItems}
-          onClickDel = {this.onClickDel}
+          onClickDel = {onClickDel}
         />
       </div>
     );
   }
-}
+
+
