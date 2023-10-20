ce817617636b4aec6e18dff4eb4943ab3ab542ed
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-10-20 21:22:38 +0200

    after_mentor

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,60 +1,62 @@
-import { StyledForm, StyledLabel, StyledBtm, StyledInput} from './AddForm.styled';
-import { useState } from "react";
+import {
+  StyledForm,
+  StyledLabel,
+  StyledBtm,
+  StyledInput,
+} from './AddForm.styled';
+import { useState } from 'react';
 
-export const AddForm = ({onFormSubmit}) => {
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+export const AddForm = ({ onFormSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
+  const onChange = evt => {
+    let nameInput = evt.currentTarget.name;
+    const { value } = evt.currentTarget;
+    nameInput === 'name' ? setName(value) : setNumber(value);
+  };
 
-    const onChangeName = evt => {
-        const { value } = evt.currentTarget;
-        setName(value)
-    }
+  const value = {
+    name: name,
+    number: number,
+  };
 
-    const onChangeNumber = evt => {
-        const { value } = evt.currentTarget;
-        setNumber(value)
-    }
+  const onSubmit = evt => {
+    evt.preventDefault();
+    onFormSubmit(value);
+    reset();
+  };
 
-    const value = {
-        name: name,
-        number: number,
-    }
-    
-    const onSubmit = evt => {
-        evt.preventDefault();
-        onFormSubmit(value);
-        reset();
-    }
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
 
-    const reset = () => {
-        setName('');
-        setNumber('');
-    }
-
-    return (
-        <StyledForm onSubmit={onSubmit}>
-        <StyledLabel>
-            Name
-            <StyledInput
-            type="text"
-            name="name"
-            // pattern="[A-Za-z]{1,32}"
-            value={name}
-            onChange={onChangeName}
-            required />
-        </StyledLabel>
-        <StyledLabel>
-            tel.
-            <StyledInput
-            type="tel"
-            name="number"
-            pattern="\+?[0-9\s\-\(\)]+"
-            value={number}
-            onChange={onChangeNumber}
-            required />
-        </StyledLabel>
-        <StyledBtm type="submit">Add contact</StyledBtm>
-        </StyledForm>
-    )
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      <StyledLabel>
+        Name
+        <StyledInput
+          type="text"
+          name="name"
+          // pattern="[A-Za-z]{1,32}"
+          value={name}
+          onChange={onChange}
+          required
+        />
+      </StyledLabel>
+      <StyledLabel>
+        tel.
+        <StyledInput
+          type="tel"
+          name="number"
+          pattern="\+?[0-9\s\-\(\)]+"
+          value={number}
+          onChange={onChange}
+          required
+        />
+      </StyledLabel>
+      <StyledBtm type="submit">Add contact</StyledBtm>
+    </StyledForm>
+  );
 };
